07a000f398b24f1a864d771e908e9a740a19c661
hendisantika <hendisantika@yahoo.co.id>
2024-11-10 06:58:20 +0700

    Add handleInternalServerException in GlobalExceptionHandler🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/webfluxr2dbc/exception/GlobalExceptionHandler.java
+++ src/main/java/id/my/hendisantika/webfluxr2dbc/exception/GlobalExceptionHandler.java
@@ -3,8 +3,13 @@ package id.my.hendisantika.webfluxr2dbc.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.reactive.function.server.EntityResponse;
 import org.springframework.web.reactive.result.method.annotation.ResponseEntityExceptionHandler;
+import reactor.core.publisher.Mono;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,4 +25,11 @@ import org.springframework.web.reactive.result.method.annotation.ResponseEntityE
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Mono<EntityResponse<ApiError>> handleInternalServerException(Exception ex) {
+        ApiError apiError = new ApiError(HttpStatus.INTERNAL_SERVER_ERROR);
+        apiError.setMessage(ex.getMessage());
+        return buildResponseEntity(apiError);
+    }
 }
