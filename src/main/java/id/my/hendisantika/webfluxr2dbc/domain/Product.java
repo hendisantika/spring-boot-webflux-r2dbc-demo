ffe88acc49d87533723e427c423f806d26b2c02b
hendisantika <hendisantika@yahoo.co.id>
2024-11-10 06:55:32 +0700

    Add Product Entity🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/webfluxr2dbc/domain/Product.java
@@ -0,0 +1,34 @@
+package id.my.hendisantika.webfluxr2dbc.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+import java.io.Serializable;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-webflux-r2dbc-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 10/11/24
+ * Time: 06.55
+ * To change this template use File | Settings | File Templates.
+ */
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product implements Serializable {
+    @Id
+    private Long id;
+    private String productName;
+    private String productType;
+    private Double price;
+    private Integer quantity;
+}
