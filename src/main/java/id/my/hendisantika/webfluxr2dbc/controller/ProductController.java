97c03250b2f1369bede88e5b91e82412c640460e
hendisantika <hendisantika@yahoo.co.id>
2024-11-10 07:12:15 +0700

    Add createProduct in ProductController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/webfluxr2dbc/controller/ProductController.java
+++ src/main/java/id/my/hendisantika/webfluxr2dbc/controller/ProductController.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,5 +40,11 @@ public class ProductController {
     @ResponseStatus(HttpStatus.OK)
     public Mono<Product> getProductById(@PathVariable("id") Long id) {
         return productService.getProductById(id);
+    }
+
+    @PostMapping("/product")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<Product> createProduct(@RequestBody Product product) {
+        return productService.addProduct(product);
     }
 }
