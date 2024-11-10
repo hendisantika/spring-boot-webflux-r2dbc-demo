638e3790389a4f541fd1b5982e4fbf1eacffbd96
hendisantika <hendisantika@yahoo.co.id>
2024-11-10 07:04:27 +0700

    Add ProductService🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/webfluxr2dbc/service/ProductService.java
@@ -0,0 +1,23 @@
+package id.my.hendisantika.webfluxr2dbc.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-webflux-r2dbc-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 10/11/24
+ * Time: 07.03
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+    private final ProductRepository productRepository;
+    private final ProductMapper productMapper;
+}
